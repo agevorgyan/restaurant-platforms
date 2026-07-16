871e668a2c22670867dcc10b152843a052052724
agevorgyan <avet.gevorgyan@gmail.com>
2026-07-16 16:10:35 +0400

    Platform foundation completed

--- apps/platform/.next/server/middleware-build-manifest.js
+++ apps/platform/.next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/5rDXEOq2OonLAlpa8710w/_buildManifest.js",
-    "static/5rDXEOq2OonLAlpa8710w/_ssgManifest.js",
-    "static/5rDXEOq2OonLAlpa8710w/_clientMiddlewareManifest.js"
+    "static/ZQNOd5jtLzuIneqNNdkTB/_buildManifest.js",
+    "static/ZQNOd5jtLzuIneqNNdkTB/_ssgManifest.js",
+    "static/ZQNOd5jtLzuIneqNNdkTB/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/0x0_qhu5ravxu.js",
